c8aa8ea1e1fa34c1751d0ec825ae08c7806e8a47
Sergey Tovt <st@norse.co>
2019-02-20 10:52:10 +0200

    Initial commit

---
+++ models/asteroidfloor.js
@@ -0,0 +1,41 @@
+{
+
+"metadata":
+{
+"sourceFile": "asteroid.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 4,
+"normals": 2,
+"colors": 0,
+"uvs": 8,
+"triangles": 2,
+"materials": 1
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "02 - Default",
+"colorDiffuse"  : [0.5882, 0.5882, 0.5882],
+"colorAmbient"  : [0.5882, 0.5882, 0.5882],
+"colorSpecular"  : [0.9000, 0.9000, 0.9000],
+"transparency"  : 1.0,
+"specularCoef"  : 10.0,
+"mapDiffuse"    : "CruiserFloor.jpg",
+"vertexColors" : false
+}
+
+],
+
+"vertices": [-68.0602,268.734,33.5413,121.94,268.734,33.5413,-68.0602,268.734,-156.459,121.94,268.734,-156.459],
+
+"normals": [0.0,1.0,0.0,0.0,1.0,0.0],
+
+"colors": [],
+
+"uvs": [[0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0]],
+
+"faces": [42,2,0,3,0,6,4,7,0,0,0,42,1,3,0,0,5,7,4,1,1,1]
+
+}
